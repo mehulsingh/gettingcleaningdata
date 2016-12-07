74fac3282765ebb07ee12d993a8805677ec19a35
mehulsingh <mehulsingh@gmail.com>
2016-12-07 14:56:04 -0500

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -4,11 +4,11 @@ filename <- "getdata_dataset.zip"
 
 ## Download and unzip the dataset:
 if (!file.exists(filename)){
-  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
-  download.file(fileURL, filename, method="curl")
+    fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
+    download.file(fileURL, filename, method="curl")
 }  
 if (!file.exists("UCI HAR Dataset")) { 
-  unzip(filename) 
+    unzip(filename) 
 }
 
 # Load activity labels + features
